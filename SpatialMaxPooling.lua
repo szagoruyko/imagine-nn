1941fe8f3a0ae0c8e05e24ebc3a7aec61255acd1
Sergey Zagoruyko <zagoruyko2@gmail.com>
2015-08-18 14:57:56 +0200

    max-pooling ceil by default

--- SpatialMaxPooling.lua
+++ SpatialMaxPooling.lua
@@ -1,2 +1,7 @@
 -- for backward compatibility with saved models
 local SpatialMaxPooling, parent = torch.class('inn.SpatialMaxPooling', 'nn.SpatialMaxPooling')
+
+function SpatialMaxPooling:__init(...)
+  parent.__init(self,...)
+  self:ceil()
+end
