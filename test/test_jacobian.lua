bbafb571b8807c18b603e617b232b2ca1c4fb0f1
Sergey Zagoruyko <zagoruyko2@gmail.com>
2015-12-25 03:41:06 -0300

    spp concat

--- test/test_jacobian.lua
+++ test/test_jacobian.lua
@@ -46,6 +46,40 @@ function inntest.SpatialStochasticPooling()
 end
 ]]--
 
+function inntest.SpatialPyramidPooling()
+   local from = math.random(1,5)
+   local ki = math.random(1,4)
+   local kj = math.random(1,4)
+   local si = math.random(1,3)
+   local sj = math.random(1,3)
+   local outi = math.random(4,5)
+   local outj = math.random(4,5)
+   local ini = (outi-1)*si+ki
+   local inj = (outj-1)*sj+kj
+
+   local module = inn.SpatialPyramidPooling({4,4},{3,3}):cuda()
+   local input = torch.rand(from,ini,inj):cuda()
+
+   local err = jac.testJacobian(module, input, nil, nil, 1e-3)
+   mytester:assertlt(err, precision, 'error on state ')
+
+   local ferr, berr = jac.testIO(module, input)
+   mytester:asserteq(ferr, 0, torch.typename(module) .. ' - i/o forward err ')
+   mytester:assertlt(berr, 1e-6, torch.typename(module) .. ' - i/o backward err ')
+
+   -- batch
+   local nbatch = math.random(2,5)
+   input = torch.rand(nbatch,from,ini,inj):cuda()
+   module = inn.SpatialPyramidPooling({4,4},{3,3}):cuda()
+
+   local err = jac.testJacobian(module, input, nil, nil, 1e-3)
+   mytester:assertlt(err, precision, 'error on state (Batch) ')
+
+   local ferr, berr = jac.testIO(module, input)
+   mytester:asserteq(ferr, 0, torch.typename(module) .. ' - i/o forward err (Batch) ')
+   mytester:assertlt(berr, 1e-6, torch.typename(module) .. ' - i/o backward err (Batch) ')
+end
+
 function inntest.SpatialCrossResponseNormalization()
     local from = 16
     local inj = 2
