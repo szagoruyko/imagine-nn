722f0beae888bbb88a189153d1892117c9531271
Sergey Zagoruyko <zagoruyko2@gmail.com>
2015-12-26 06:45:28 +0100

    remove dimd

--- SpatialPyramidPooling.lua
+++ SpatialPyramidPooling.lua
@@ -5,7 +5,6 @@ function SpatialPyramidPooling:__init(...)
    parent.__init(self, 1)
    local pyr = {...}
    self.pyr = torch.type(pyr[1][1]) == 'table' and pyr[1] or pyr
-   self.dimd = 1
    for k,v in ipairs(self.pyr) do
       parent.add(self, nn.Sequential()
          :add(nn.SpatialAdaptiveMaxPooling(v[1], v[2]))
@@ -15,24 +14,9 @@ function SpatialPyramidPooling:__init(...)
    end
 end
 
-function dimensionSet(self, input)
-   local dimd = 1
-   if input:nDimension() == 4 then 
-      dimd = dimd + 1
-   end
-
-   if self.dimd ~= dimd then
-      self.dimd = dimd
-      self.dimension = dimd
-   end
-end
-
 function SpatialPyramidPooling:updateOutput(input)
-   dimensionSet(self, input) 
+   assert(input:dim() == 4 or input:dim() == 3, 'unsupported dimensionality')
+   self.dimension = input:dim() - 2
    return parent.updateOutput(self, input)
 end
 
-function SpatialPyramidPooling:updateGradInput(input, gradOutput)
-   dimensionSet(self, input) 
-   return parent.updateGradInput(self, input, gradOutput)
-end
